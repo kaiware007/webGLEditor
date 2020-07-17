1b5301b250038cd195f53131282e6d0f1fcb3bd7
matsudayuma <>
2020-07-17 20:27:22 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
     <router-view/>
-    <glslEditor/>
   </div>
 </template>
 
 <script>
-import glslEditor from '@/components/glslEditor'
 
 export default {
-  name: 'App',
-  components: {
-    glslEditor
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
